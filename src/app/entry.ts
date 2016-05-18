1f978aaeb7ac5abef61f081311c4a06cb4d838cd
Marais Rossouw <me@maraisr.com>
2016-05-18 21:37:08 +1000

    chore: starting out a pocketsmit feed reader

--- src/app/entry.ts
+++ src/app/entry.ts
@@ -1,17 +1,50 @@
 declare var require: any;
 
+import * as Vue from 'vue';
+
 import Component from 'vue-class-component';
+import * as VueResource from 'vue-resource';
+
+import Config from './config';
+
+Vue.use(VueResource);
+Vue.http.options.root = 'https://api.pocketsmith.com/v2';
+Vue.http.headers.common['Authorization'] = `Bearer ${Config.pocketsmithApi}`;
 
 @Component({
-	el: () => {return '#app'},
+	el: () => { return '#app' },
 	template: require('./views/entry.pug')
 })
+class App extends Vue {
+	private userData: any;
 
-class App {
-	data():any {
+	data(): any {
 		return {
-			name: 'World'
-		}
+			loaded: false,
+			user: void 0,
+			transactions: []
+		};
+	}
+
+	ready(): void {
+		// Get the authed user
+		this.$http.get('me')
+			.then((resp) => {
+				if (resp.status == 200) {
+					this.userData = resp.data;
+					this.$set('user', resp.data);
+
+					this.getTransactions();
+				}
+			});
+	}
+
+	private getTransactions() {
+		this.$http.get(`users/${this.userData.id}/transactions`)
+			.then((resp) => {
+				this.$set('transactions', resp.data);
+				this.$set('loaded', true);
+			})
 	}
 }
 
